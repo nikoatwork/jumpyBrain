475160188cb3f0a3cb9e5925243d5231d29fa685
nikoatwork <nikoatwork@pm.me>
2026-06-24 20:56:39 +0200

    feat: US-003 - Introduce local runtime composition module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
-export * from "./core/index.js";
-export { processMemory } from "./processing/index.js";
-export { indexMemory, searchMemory } from "./retrieval/index.js";
+export * from "./runtime/index.js";
